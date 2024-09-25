9c54261c534617b664153eaec0fab91b0104b73f
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-09-25 14:32:43 +0200

    Tout fonctionne

--- Exo 1/script.js
+++ Exo 1/script.js
@@ -1,6 +1,14 @@
 const square = document.getElementById("square");
 
-square.addEventListener("click", ()=>{
-    alert("test");
+var className = square.className;
+var color = window.getComputedStyle(square).color;
+var backgroundColor = window.getComputedStyle(square).backgroundColor;
+var height = square.offsetHeight;
+var width = square.offsetWidth;
+var fontFamily = window.getComputedStyle(square).fontFamily;
+var fontSize = window.getComputedStyle(square).fontSize;
+
+square.addEventListener("click", () => {
+    alert("Class : " + className + "\n" + "Background color : " + backgroundColor + "\n" + "Color : " + color + "\n" + "Height : " + height + "px" + "\n" + "Width : " + width + "px" + "\n" + "Display : " + '???' + "\n" + "Display : " + fontFamily + " (" + fontSize + ")");
 });
 
