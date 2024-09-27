cbd3cdecad91e9803f3640fb4ba503fb6c81a53c
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-09-27 15:08:58 +0200

    Fonctionnel Fini
    Partie calcul direct finie Changer CSS

--- Exo 5/scripts.js
+++ Exo 5/scripts.js
@@ -3,7 +3,6 @@ const output = document.getElementById("output");
 const form = document.getElementById("form");
 
 form.addEventListener("input", () => {
-    // alert(form.value);
     var amount = form.value;
     output.innerText = (amount*6.55957).toFixed(2) + " francs";
 });
